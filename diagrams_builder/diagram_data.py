c1053cfb91e65c1e35e81d1325e6ead18c6b8012
Nikita <mykytamaliarenko@gmail.com>
2019-12-11 18:44:29 +0200

    feat(diagram_builder.diagram_data): add to_json

--- diagrams_builder/diagram_data.py
+++ diagrams_builder/diagram_data.py
@@ -1,3 +1,6 @@
+import json
+
+
 class DiagramData:
     points: list
     links: list
@@ -7,4 +10,13 @@ class DiagramData:
         self.links = links
 
     def to_json(self) -> str:
-        pass
+        points_json = list()
+        [points_json.append(el.to_dict()) for el in self.points]
+
+        links_json = list()
+        [links_json.append(el.to_dict()) for el in self.links]
+
+        return json.dumps({
+            "points": points_json,
+            "links": links_json
+        })
