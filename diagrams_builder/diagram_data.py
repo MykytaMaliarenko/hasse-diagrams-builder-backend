6faf9a20ec98ef4fc4d80c3434b9570f62519599
Nikita <mykytamaliarenko@gmail.com>
2019-12-12 21:27:49 +0200

    ref(diagrams_builder.diagram_data): change to_json to to_dict

--- diagrams_builder/diagram_data.py
+++ diagrams_builder/diagram_data.py
@@ -9,14 +9,14 @@ class DiagramData:
         self.points = points
         self.links = links
 
-    def to_json(self) -> str:
+    def to_dict(self) -> dict:
         points_json = list()
         [points_json.append(el.to_dict()) for el in self.points]
 
         links_json = list()
         [links_json.append(el.to_dict()) for el in self.links]
 
-        return json.dumps({
+        return {
             "points": points_json,
             "links": links_json
-        })
+        }
