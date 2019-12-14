bc4e8175b818d5af5f9c6c99a98a69a1157962c3
Nikita <mykytamaliarenko@gmail.com>
2019-12-14 21:44:18 +0200

    feat(main): add cors headers

--- main.py
+++ main.py
@@ -8,5 +8,14 @@ api = Api(app)
 
 api.add_resource(handler.DiagramBuilder, "/diagramBuilder")
 
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
+
 if __name__ == '__main__':
     app.run(debug=True)
